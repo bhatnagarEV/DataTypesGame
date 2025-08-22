b90537e203ffaae80727f4108171be661f9b8715
Sangeeta Bhatnagar <bhatnagars@esuhsd.org>
2025-08-22 00:17:11 -0700

    Use root base for Netlify

--- DataTypesGame/vite.config.ts
+++ DataTypesGame/vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/DataTypesGame/',   // 👈 must match your repo name exactly
+//  base: '/DataTypesGame/',   // 👈 must match your repo name exactly
 })
